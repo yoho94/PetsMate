08b7c45129463f05285ac31fda204da43a988d1e
KSD94 <aay32@naver.com>
2019-05-11 00:37:06 +0900

    TEST

--- app/src/main/java/com/example/petsmate/MainActivity.java
+++ app/src/main/java/com/example/petsmate/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+    // TEST ! 왼쪽에 보면 파란색으로 바뀜 이게 수정사항이 있으면 파란색불 들어와
+    // 이제 이걸 깃허브에 업로드 하려면 ADD는 추가 파일이 생기면 ADD하는거
+    // 지금은 그냥 수정만해서 안해도 되긴 됨 ADD를 한 후
     // 액티비티 띄우기
     public void onSignupClick(View v) {
         Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
