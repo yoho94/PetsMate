ac26ede508f1e07167947e3a7994dc82604eed5d
smini1326 <smini1326@naver.com>
2019-05-12 22:02:25 +0900

    예약 화면 XML - 수민

--- app/src/main/java/com/example/petsmate/MainActivity.java
+++ app/src/main/java/com/example/petsmate/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+//        EditText test = (EditText) findViewById(R.id.id_input); 로그인의 ID 입력 창
+//        String id = test.getText().toString(); // 그것을 String로 바꿈 !
     }
-    // TEST ! 왼쪽에 보면 파란색으로 바뀜 이게 수정사항이 있으면 파란색불 들어와
-    // 이제 이걸 깃허브에 업로드 하려면 ADD는 추가 파일이 생기면 ADD하는거
-    // 지금은 그냥 수정만해서 안해도 되긴 됨 ADD를 한 후
+
     // 액티비티 띄우기
     public void onSignupClick(View v) {
         Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
---
+++ app/src/main/java/com/example/petsmate/ReserveMain.java
@@ -0,0 +1,13 @@
+package com.example.petsmate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ReserveMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reserve_main);
+    }
+}
