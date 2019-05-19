dc9c26580ab9c940631379eb7e3e3b6de8b47481
rlaguswns94 <rlaguswns94@naver.com>
2019-05-19 17:40:05 +0900

    예약에서 pet 불러오기 수정중

--- app/src/main/java/com/example/petsmate/ReserveMain.java
+++ app/src/main/java/com/example/petsmate/ReserveMain.java
@@ -1,50 +1,70 @@
 package com.example.petsmate;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.ListView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+class MyAdapter extends ArrayAdapter{
+    private Activity m_context;
+    private ArrayList<String> m_arrayList;
+    private ArrayList<CheckBox> m_checkBoxList;
+
+    public MyAdapter(Activity context, int textViewResourceId,
+                     ArrayList<String> objects) {
+        super(context, textViewResourceId, objects);
+
+        m_context = context;
+        m_arrayList = objects;
+        setCheckBoxList();
+    }
+
+    private void setCheckBoxList()
+    {
+        m_checkBoxList = new ArrayList<CheckBox>();
+        for(int i = 0; i < m_arrayList.size(); i++)
+        {
+            CheckBox checkBox = new CheckBox(m_context);
+            checkBox.setFocusable(false);
+            checkBox.setClickable(false);
+            checkBox.setText(m_arrayList.get(i));
+            m_checkBoxList.add(checkBox);
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        return m_checkBoxList.get(position);
+    }
+}
 
 public class ReserveMain extends AppCompatActivity {
 
-    TableLayout petLayout; // 펫 위치 시킬 레이아웃
     ArrayList<PetInfo> petInfos;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reserve_main);
-        petLayout = (TableLayout) findViewById(R.id.reserve_layout_tablePet);
 
         /**  펫 읽어와서 띄우기 **/
         petInfos = MainActivity.memberInfo.getPetInfos();
-        // 레이아웃 warp_content 지정
-        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        ArrayList<String> arrayList = new ArrayList<>();
 
         for(int i=0; i<petInfos.size(); i++) {
-            TableRow tableRow = new TableRow(this);
-            tableRow.setLayoutParams(layoutParams);
-            tableRow.setGravity(Gravity.CENTER);
-
-            for(int j=0; j<petInfos.size()/2+0.95; j++) {
-
-                CheckBox checkBox = new CheckBox(this);
-                checkBox.setLayoutParams(layoutParams);
-
-                int petCode = petInfos.get(i).getCodeInt();
-                String petName = petInfos.get(i).getName();
-
-                checkBox.setId(petCode);
-                checkBox.setText(petName);
-
-                tableRow.addView(checkBox);
-            }
+            arrayList.add(petInfos.get(i).getName());
         }
+
+        MyAdapter adapter = new MyAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView = (ListView) findViewById(R.id.reserve_petListView);
+        listView.setAdapter(adapter);
     }
 }
