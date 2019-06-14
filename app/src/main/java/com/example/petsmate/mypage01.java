142926b150b07ed6fa169acee4a869ac68370611
강지훈 <kanngji>
2019-06-15 03:21:28 +0900

    xml 버튼 화면이동

---
+++ app/src/main/java/com/example/petsmate/mypage01.java
@@ -0,0 +1,66 @@
+package com.example.petsmate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class mypage01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage01);
+
+        Button info = (Button) findViewById(R.id.info_btn);
+        info.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), mypage04.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button version = (Button) findViewById(R.id.call_btn);
+        version.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), mypage07.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button call = (Button) findViewById(R.id.call_btn);
+        call.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), mypage06.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+        Button help = (Button) findViewById(R.id.help_btn);
+        help.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), mypage03.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+
+        Button alarm =(Button)findViewById(R.id.alarm_btn);
+        alarm.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        Intent intent= new Intent(getApplicationContext(), mypage02.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/com/example/petsmate/mypage07.java
@@ -0,0 +1,13 @@
+package com.example.petsmate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class mypage07 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage07);
+    }
+}
