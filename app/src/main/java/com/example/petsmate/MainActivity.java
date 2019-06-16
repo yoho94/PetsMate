072c290727c85b0a715d07425125d0377da85336
JISUN <js940616@naver.com>
2019-06-16 12:51:49 +0900

    네이버로 로그인 버튼 추가

--- app/src/main/java/com/example/petsmate/MainActivity.java
+++ app/src/main/java/com/example/petsmate/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.petsmate;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
+
+import static com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler;
+
 
 class MemberInfo { // 회원의 정보를 저장할 클래스.
     private String id; // 아이디
@@ -129,40 +139,97 @@ class MemberInfo { // 회원의 정보를 저장할 클래스.
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String OAUTH_CLIENT_ID="7nlmp3_oTZszqdkLgv2u";
+    private static String OAUTH_CLIENT_SECRET="e8WuNOzDTI";
+    private static String OAUTH_CLIENT_NAME="네이버 아이디로 로그인";
+
+    public static OAuthLoginButton mOAuthLoginButton;
+    public static OAuthLogin mOAuthLoginInstance;
+
+    public static Context mContext;
+
+
+
     static MemberInfo memberInfo = new MemberInfo();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //context저장
+        mContext = MainActivity.this;
+
+        //1.초기화
+        mOAuthLoginInstance = OAuthLogin.getInstance();
+        mOAuthLoginInstance.showDevelopersLog(true);
+        mOAuthLoginInstance.init(mContext, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME);
+
+        //2.로그인버튼셋팅
+        mOAuthLoginButton = (OAuthLoginButton) findViewById(R.id.buttonNaverLogin);
+        mOAuthLoginButton.setOAuthLoginHandler(mOAuthLoginHandler);
+
+
         Intent intent = getIntent();
-        Button loginButton = (Button)findViewById(R.id.login);
-        Button userjoinButton = (Button)findViewById(R.id.user_join);
-        Button driverjoinButton=(Button)findViewById(R.id.driver_join);
+        Button loginButton = (Button) findViewById(R.id.login);
+        Button userjoinButton = (Button) findViewById(R.id.user_join);
+        Button driverjoinButton = (Button) findViewById(R.id.driver_join);
+
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
         userjoinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivity(intent);
             }
         });
         driverjoinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),DriverSignupActivity.class);
+                Intent intent = new Intent(getApplicationContext(), DriverSignupActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
+
+
+
+
+    }
+    static private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            //로그인인증성공
+
+
+
+            if (success) {
+                //사용자정보가져오기
+                String accessToken = mOAuthLoginInstance.getAccessToken(mContext);
+                String refreshToken = mOAuthLoginInstance.getRefreshToken(mContext);
+                long expitesAt = mOAuthLoginInstance.getExpiresAt(mContext);
+                String tokenType = mOAuthLoginInstance.getTokenType(mContext);
+
+                Intent intent = new Intent(this, DriverSignupActivity.class);
+                startActivity(intent);
+
+
+            } else {
+                //실패
+                String errorCode = mOAuthLoginInstance.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode + ",errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    };
 
     // 액티비티 띄우기
     public void onSignupClick(View v) { // 이용자등록 클릭
