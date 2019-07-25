be14394adaed2d3cd9eb3b1bff5607f336c71db0
kanngji <jihoonkr05@naver.com>
2019-07-25 18:25:31 +0900

    채팅창 (firebase import 시킴)

--- app/src/main/java/com/example/petsmate/ChatActivity.java
+++ app/src/main/java/com/example/petsmate/ChatActivity.java
@@ -1,14 +1,15 @@
 package com.example.petsmate;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-
 
 
 import com.google.firebase.database.ChildEventListener;
