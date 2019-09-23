89e5d8620957e881f370a388aa5b0ca834b74234
guddid <sixxthreee@gmail.com>
2019-09-23 10:34:44 +0900

    마이페이지-마이리뷰 관리 추가

---
+++ app/src/main/java/com/example/petsmate/task/mypage_review.java
@@ -0,0 +1,22 @@
+package com.example.petsmate.task;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+
+import com.example.petsmate.BaseActivity;
+import com.example.petsmate.R;
+
+import org.jsoup.Connection;
+
+public class mypage_review extends BaseActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_review);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+        configBottomNavigation(this, bottomNavigationView);
+    }
+}
