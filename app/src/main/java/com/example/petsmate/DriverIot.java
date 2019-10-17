ec3cc2ad1f29de0ff1363a9d911444b282779250
yoho94 <50386803+yoho94@users.noreply.github.com>
2019-10-17 17:24:09 +0900

    기사 IoT 화면에서 받아온 정보(GPS)를 맵에 표기하도록 변경.

--- app/src/main/java/com/example/petsmate/DriverIot.java
+++ app/src/main/java/com/example/petsmate/DriverIot.java
@@ -9,8 +9,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
@@ -25,15 +28,40 @@ import com.example.petsmate.ble.BluetoothLeService;
 import com.example.petsmate.ble.DeviceControlActivity;
 import com.example.petsmate.ble.DeviceScanActivity;
 import com.example.petsmate.table.IotTable;
+import com.example.petsmate.task.InsertIotTask;
+import com.example.petsmate.task.SelectIotTask;
+import com.github.florent37.singledateandtimepicker.dialog.DoubleDateAndTimePickerDialog;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.util.MarkerIcons;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class DriverIot extends BaseActivity {
+public class DriverIot extends BaseActivity implements OnMapReadyCallback {
 
     private ArrayList<IotTable> iotTables;
+    private MapFragment mapFragment;
+    private NaverMap naverMap = null;
+    private ArrayList<Marker> markers;
+    private ImageButton refreshBtn;
+    private InfoWindow infoWindow;
+    private FusedLocationSource locationSource;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
     private Context context;
     private ImageButton blueBtn;
@@ -171,7 +199,72 @@ public class DriverIot extends BaseActivity {
                     Log.i("onSerialReceived", intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
 //                    onSerialReceived(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
                 }
-                System.out.println("displayData "+intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+                try {
+                    String tmp = intent.getStringExtra(BluetoothLeService.EXTRA_DATA);
+                    System.out.println("displayData " + intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+
+                    if (tmp != null || !tmp.isEmpty()) {
+                        data.append(tmp);
+                        Log.i("data", data.toString());
+                        if (!data.toString().startsWith("Lat:")) {
+                            data = new StringBuilder();
+                        } else {
+                            String[] dataArr = data.toString().split("\n");
+                            if (dataArr.length == 6) {
+
+                                IotTable iotTable = new IotTable();
+                                String latitude, longitude, date, hert;
+                                Integer pet_code, heart_rate;
+                                Timestamp generate_time;
+                                latitude = dataArr[0].substring(5).trim();
+                                longitude = dataArr[1].substring(5).trim();
+                                date = dataArr[2].substring(6) + " " + dataArr[3].substring(6).trim();
+                                hert = dataArr[5].substring(5).trim();
+
+                                Log.i("DATE", date);
+
+
+                                if (hert.isEmpty())
+                                    heart_rate = 0;
+                                else
+                                    heart_rate = Integer.parseInt(hert);
+
+                                if (date.isEmpty() || 14 > date.length())
+                                    generate_time = new Timestamp(System.currentTimeMillis());
+//                                    generate_time = null;
+                                else
+                                    generate_time = new Timestamp(System.currentTimeMillis());
+//                                    generate_time = Timestamp.valueOf("20"+date);
+
+                                iotTable.setLatitude(latitude);
+                                iotTable.setLongitude(longitude);
+                                iotTable.setGenerate_time(generate_time);
+                                iotTable.setHeart_rate(heart_rate);
+                                iotTable.setPet_code(69); // TEST TODO 실제 펫 코드, 아이디 받기.
+                                iotTable.setId("Test"); // TEST
+
+                                Log.i("iotTable", iotTable.toString());
+
+
+                                if (iotTable.getLatitude().isEmpty() || iotTable.getLongitude().isEmpty()) {
+                                    Log.i("insertIot", "gps isEmpty");
+                                } else {
+                                    iotTables.add(iotTable);
+                                    setIotMarker();
+                                }
+
+//                                Toast.makeText(getApplicationContext(), iotTable.toString(), Toast.LENGTH_SHORT).show();
+
+                                data = new StringBuilder();
+                            }
+
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e("IoT에러(DriverIOT)", e.toString());
+                    data = new StringBuilder();
+                    setIotMarker();
+                }
 
 //                try {
 //                    String tmp = intent.getStringExtra(BluetoothLeService.EXTRA_DATA);
@@ -450,7 +543,6 @@ public class DriverIot extends BaseActivity {
                 .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH)
                 .check();
 
-        iotTables = new ArrayList<>();
 
         BottomNavigationView bottomNavigationView;
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
@@ -466,6 +558,61 @@ public class DriverIot extends BaseActivity {
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
+
+        markers = new ArrayList<>();
+        iotTables = new ArrayList<>();
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+        mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.driver_iot_map);
+        mapFragment.getMapAsync(this);
+
+        // 마커의 정보창
+        infoWindow = new InfoWindow();
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(mapFragment.getContext()) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return  (CharSequence)infoWindow.getMarker().getTag();
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(@Nullable NaverMap naverMap) {
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+        this.naverMap = naverMap;
+
+//        Marker marker = new Marker();
+//        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+//        marker.setMap(naverMap);
+
+        try {
+            setIotMarker();
+
+        } catch (Exception e) {
+            Log.e("iotgps", e.toString());
+        }
+
+        // 새로고침 버튼
+        refreshBtn = (ImageButton) findViewById(R.id.driver_iot_refresh_btn);
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIotMarker();
+            }
+        });
+
+        naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+                infoWindow.close();
+            }
+        });
+
 
     }
 
@@ -483,4 +630,61 @@ public class DriverIot extends BaseActivity {
             }
         }
     }
+
+    private void setIotMarker() {
+
+        for (int i = 0; i < markers.size(); i++) { // 기존의 마커 삭제
+            markers.get(i).setMap(null);
+        }
+        markers.clear();
+
+        if(iotTables.isEmpty())
+            return;
+
+        for (int i = 0; i < iotTables.size(); i++) {
+            final IotTable iotTable = iotTables.get(i);
+
+
+            final Marker marker = new Marker();
+            LatLng latLng = new LatLng(Double.parseDouble(iotTable.getLatitude()), Double.parseDouble(iotTable.getLongitude()));
+            Log.i("Marker", latLng.toString());
+
+            if(i == iotTables.size() -1)
+                marker.setIcon(OverlayImage.fromResource(R.drawable.ic_dog_marker));
+            else {
+                marker.setIcon(MarkerIcons.BLACK); // 마커 아이콘을 블랙으로
+                marker.setCaptionText(i+1+"");
+            }
+            marker.setIconTintColor(Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));  // 마커 색상 랜덤
+            marker.setAlpha(0.75f); // 마커 반투명으로 설정
+            marker.setTag("시간 : " + iotTable.getGenerate_time() + "\n" + "심박수 : " + iotTable.getHeart_rate());
+            marker.setOnClickListener(new Overlay.OnClickListener() {
+                @Override
+                public boolean onClick(@NonNull Overlay overlay) {
+                    infoWindow.open(marker);
+                    return true;
+                }
+            });
+
+
+//                marker.setCaptionRequestedWidth(250); // 마커의 캡션 넓이(줄바꿈)
+//                marker.setCaptionColor(Color.BLUE); // 마커 캡션 색상
+//                marker.setCaptionHaloColor(Color.rgb(170, 255, 170)); // 마커 캡션 겉 색상
+//                marker.setCaptionText("목적지 : " + callTable.destinationPlace.getRoadAddress()); // 마커 캡션 목적지 표기
+
+            marker.setPosition(latLng);  // 마커 위치 설정
+            marker.setMap(naverMap);    // 마커 맵에 표기
+
+            markers.add(marker); // 마커 관리 리스트. (갱신 시 삭제를 위해)
+            infoWindow.open(marker);
+        }
+
+        double latitude = Double.parseDouble(iotTables.get(iotTables.size()-1).getLatitude());
+        double longitude = Double.parseDouble(iotTables.get(iotTables.size()-1).getLongitude());
+
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(latitude, longitude));
+        naverMap.moveCamera(cameraUpdate); // 마지막 펫 위치로 지도 옮기기.
+
+    }
+
 }
